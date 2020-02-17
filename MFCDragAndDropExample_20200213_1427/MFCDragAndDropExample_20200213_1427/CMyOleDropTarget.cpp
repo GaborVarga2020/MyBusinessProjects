e30918ac7e1ffa7fb0f53c66771c97cc0d9a4955
Gábor Varga <gabor.varga.1975@gmail.com>
2020-02-17 15:42:56 +0100

    2020.02.17. 15.42.

--- MFCDragAndDropExample_20200213_1427/MFCDragAndDropExample_20200213_1427/CMyOleDropTarget.cpp
+++ MFCDragAndDropExample_20200213_1427/MFCDragAndDropExample_20200213_1427/CMyOleDropTarget.cpp
@@ -11,25 +11,138 @@ CMyOleDropTarget::CMyOleDropTarget()
 {
 }
 
+// (Implemented by VargaG., on 2020.02.17.)
+BOOL CMyOleDropTarget::IsFilePathNameListIsDragged(COleDataObject* a_pDataObject)
+{
+	// (Implemented by VargaG., on 2020.02.17.)
+	m_droppedFilePathNames.RemoveAll();
+
+	// (Implemented by VargaG., on 2020.02.17.)
+	CLIPFORMAT clipFormat = CF_HDROP;
+	HGLOBAL hglobalDropFiles = a_pDataObject->GetGlobalData(clipFormat);
+
+	// (Implemented by VargaG., on 2020.02.17.)
+	__try
+	{
+		// (Implemented by VargaG., on 2020.02.17.)
+		LPDROPFILES pDropFiles = (LPDROPFILES)::GlobalLock(hglobalDropFiles);
+		if (pDropFiles == NULL)
+		{
+			// (Implemented by VargaG., on 2020.02.17.)
+			ASSERT(FALSE);
+			return FALSE;
+		}
+
+		// (Implemented by VargaG., on 2020.02.17.)
+		if (pDropFiles->fWide)
+		{
+			// (Implemented by VargaG., on 2020.02.17.)
+			bool bAdditionalFileNameExistst = true;
+
+			// (Implemented by VargaG., on 2020.02.17.)
+			PWCHAR pDroppedFilePathAndName = (PWCHAR)((PCHAR)pDropFiles + pDropFiles->pFiles);
+
+			do
+			{
+				// (Implemented by VargaG., on 2020.02.17.)
+				m_droppedFilePathNames.Add(pDroppedFilePathAndName);
+
+				// Get the number of characters in the string, not including the terminating null character.
+				// (Implemented by VargaG., on 2020.02.17.)
+				size_t nDroppedFilePathAndNameLengthWithoutTerminatingNullCharacter = wcsnlen_s(pDroppedFilePathAndName, MAX_PATH);
+
+				// (Implemented by VargaG., on 2020.02.17.)
+				pDroppedFilePathAndName = pDroppedFilePathAndName + nDroppedFilePathAndNameLengthWithoutTerminatingNullCharacter + 1;
+
+				// (Implemented by VargaG., on 2020.02.17.)
+				bAdditionalFileNameExistst = (*pDroppedFilePathAndName != L'\x0');
+			}
+			while (bAdditionalFileNameExistst);
+		}
+		else
+		{
+			// (Implemented by VargaG., on 2020.02.17.)
+			TRACE("ANSI characters are not supported, only Unicode characters!");
+			ASSERT(FALSE);
+		}
+
+		return TRUE;
+	}
+	__finally
+	{
+		// (Implemented by VargaG., on 2020.02.17.)
+		BOOL bGlobalUnlockResult = ::GlobalUnlock(hglobalDropFiles);
+		DWORD dwLastError = GetLastError();
+		if ((bGlobalUnlockResult == FALSE) && (dwLastError != NO_ERROR))
+		{
+			// (Implemented by VargaG., on 2020.02.17.)
+			ASSERT(FALSE);
+			return FALSE;
+		}
+	}
+
+	return FALSE;
+}
+
 // (Implemented by VargaG., on 2020.02.13.)
 DROPEFFECT CMyOleDropTarget::OnDragEnter(CWnd* a_pWnd, COleDataObject* a_pDataObject, DWORD a_dwKeyState, CPoint a_point)
 {
-/*
-Possible return values.
-(Implemented by VargaG., on 2020.02.17.)
+	// (Implemented by VargaG., on 2020.02.17.)
+	DROPEFFECT dropEffect = DROPEFFECT_NONE;
 
-	DROPEFFECT_NONE
-	DROPEFFECT_COPY
-	DROPEFFECT_MOVE
-	DROPEFFECT_LINK
-	DROPEFFECT_SCROLL
-*/
+	// (Implemented by VargaG., on 2020.02.17.)
+	if (IsFilePathNameListIsDragged(a_pDataObject))
+	{
+		// (Implemented by VargaG., on 2020.02.17.)
+		dropEffect = DROPEFFECT_COPY;
+	}
 
-	return DROPEFFECT_NONE;
+	// (Implemented by VargaG., on 2020.02.17.)
+	return dropEffect;
+}
+
+// (Implemented by VargaG., on 2020.02.17.)
+DROPEFFECT CMyOleDropTarget::OnDragOver(CWnd* a_pWnd, COleDataObject* a_pDataObject, DWORD a_dwKeyState, CPoint a_point)
+{
+	// (Implemented by VargaG., on 2020.02.17.)
+	DROPEFFECT dropEffect = DROPEFFECT_NONE;
+
+	// (Implemented by VargaG., on 2020.02.17.)
+	if (IsFilePathNameListIsDragged(a_pDataObject))
+	{
+		// (Implemented by VargaG., on 2020.02.17.)
+		dropEffect = DROPEFFECT_COPY;
+	}
+
+	// (Implemented by VargaG., on 2020.02.17.)
+	return dropEffect;
 }
 
 // (Implemented by VargaG., on 2020.02.13.)
 BOOL CMyOleDropTarget::OnDrop(CWnd* a_pWnd, COleDataObject* a_pDataObject, DROPEFFECT a_dropEffect, CPoint a_point)
 {
-	return TRUE;
+	// (Implemented by VargaG., on 2020.02.17.)
+	BOOL bDropAllowed = IsFilePathNameListIsDragged(a_pDataObject);
+
+	// (Implemented by VargaG., on 2020.02.17.)
+	if (bDropAllowed)
+	{
+		// (Implemented by VargaG., on 2020.02.17.)
+		TRACE("Number of files have been dropped: %d.\n", m_droppedFilePathNames.GetCount());
+
+		// (Implemented by VargaG., on 2020.02.17.)
+		for (int nDroppedFileDataIndex = 0; nDroppedFileDataIndex < m_droppedFilePathNames.GetCount(); nDroppedFileDataIndex++)
+		{
+			// (Implemented by VargaG., on 2020.02.17.)
+			CString stringFileDataIndexed = m_droppedFilePathNames.GetAt(nDroppedFileDataIndex);
+			TRACE(
+				"Dropped file path and name (%d of %d): %s",
+				nDroppedFileDataIndex + 1,
+				m_droppedFilePathNames.GetCount(),
+				stringFileDataIndexed
+			);
+		}
+	}
+
+	return bDropAllowed;
 }
--- MFCDragAndDropExample_20200213_1427/MFCDragAndDropExample_20200213_1427/CMyOleDropTarget.h
+++ MFCDragAndDropExample_20200213_1427/MFCDragAndDropExample_20200213_1427/CMyOleDropTarget.h
@@ -8,9 +8,13 @@
 // (Implemented by VargaG., on 2020.02.13.)
 class CMyOleDropTarget : public COleDropTarget
 {
+private: CStringArray m_droppedFilePathNames;		// (Implemented by VargaG., on 2020.02.17.)
+
 public: CMyOleDropTarget();		// (Implemented by VargaG., on 2020.02.13.)
 public: virtual ~CMyOleDropTarget();		// (Implemented by VargaG., on 2020.02.13.)
+public: BOOL IsFilePathNameListIsDragged(COleDataObject* a_pDataObject);		// (Implemented by VargaG., on 2020.02.17.)
 public: virtual DROPEFFECT OnDragEnter(CWnd* a_pWnd, COleDataObject* a_pDataObject, DWORD a_dwKeyState, CPoint a_point);		// (Implemented by VargaG., on 2020.02.13.)
+public: virtual DROPEFFECT OnDragOver(CWnd* a_pWnd, COleDataObject* a_pDataObject, DWORD a_dwKeyState, CPoint a_point);		// (Implemented by VargaG., on 2020.02.17.)
 public: virtual BOOL OnDrop(CWnd* a_pWnd, COleDataObject* a_pDataObject, DROPEFFECT a_dropEffect, CPoint a_point);		// (Implemented by VargaG., on 2020.02.13.)
 };
 
