98ab58dcabf5fe2d2b407a8cc2d57a4ba7797c84
Gábor Varga <gabor.varga.1975@gmail.com>
2020-12-04 11:14:41 +0100

    2020.12.04. 11.14.

---
+++ EntityFrameworkDemoApplication/EntityFrameworkDataAccessLibrary/DataAccess/PeopleContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkDataAccessLibrary.DataAccess
+{
+	public class PeopleContext : Microsoft.EntityFrameworkCore.DbContext
+	{
+		#region Fields/Properties
+
+		public Microsoft.EntityFrameworkCore.DbSet<Models.Address> Addresses { get; set; }
+
+		public Microsoft.EntityFrameworkCore.DbSet<Models.Email> EmailAddresses { get; set; }
+
+		public Microsoft.EntityFrameworkCore.DbSet<Models.Person> People { get; set; }
+
+		#endregion
+
+		#region Constructors
+
+		public PeopleContext(Microsoft.EntityFrameworkCore.DbContextOptions a_dbContextOptions)
+			: base(a_dbContextOptions)
+		{
+		}
+
+		#endregion
+	}
+}
